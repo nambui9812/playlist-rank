b26fe79a9eb97261cd6795cd822685454b6f8eae
nambui9812 <buihainam9812@gmail.com>
2020-02-19 09:37:37 -0700

    first commit

---
+++ playlist-rank/src/main/java/nambui9812/playlistrank/PlaylistRankApplication.java
@@ -0,0 +1,13 @@
+package nambui9812.playlistrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistRankApplication.class, args);
+	}
+
+}
---
+++ playlist-rank/src/test/java/nambui9812/playlistrank/PlaylistRankApplicationTests.java
@@ -0,0 +1,13 @@
+package nambui9812.playlistrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
