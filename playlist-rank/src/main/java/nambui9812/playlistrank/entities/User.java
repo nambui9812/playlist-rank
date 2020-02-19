5364b24140367bbdc43e7e238eeb8e5f47ad2efb
nambui9812 <buihainam9812@gmail.com>
2020-02-19 12:30:18 -0700

    update user entity

--- playlist-rank/src/main/java/nambui9812/playlistrank/entities/User.java
+++ playlist-rank/src/main/java/nambui9812/playlistrank/entities/User.java
@@ -87,79 +87,159 @@ public class User {
     return firtsName;
   }
 
+  public void setFirstName(String firstName) {
+    this.firtsName = firstName;
+  }
+
   public String getLastName() {
     return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
   }
 
   public String getEmail() {
     return email;
   }
 
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public String getUsername() {
     return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
   }
 
   public String getPassword() {
     return password;
   }
 
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
   public String getDateOfBirth() {
     return dateOfBirth;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
   }
 
   public String getMonthOfBirth() {
     return monthOfBirth;
   }
 
+  public void setMonthOfBirth(String monthOfBirth) {
+    this.monthOfBirth = monthOfBirth;
+  }
+
   public String getYearOfBirth() {
     return yearOfBirth;
+  }
+
+  public void setYearOfBirth(String yearOfBirth) {
+    this.yearOfBirth = yearOfBirth;
   }
 
   public String getStreet() {
     return street;
   }
 
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
   public String getOptionalStreet() {
     return optionalStreet;
+  }
+
+  public void setOptionalStreet(String optionalStreet) {
+    this.optionalStreet = optionalStreet;
   }
 
   public String getCity() {
     return city;
   }
 
+  public void setCity(String city) {
+    this.city = city;
+  }
+
   public String getProvince() {
     return province;
+  }
+
+  public void setProvince(String province) {
+    this.province = province;
   }
 
   public String getCountry() {
     return country;
   }
 
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
   public String getZipcode() {
     return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
   }
 
   public String getPhone() {
     return phone;
   }
 
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
   public String getStatus() {
     return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
   }
 
   public ArrayList<String> getFollowingList() {
     return followingList;
   }
 
+  public void setFollowingList(ArrayList<String> followingList) {
+    this.followingList = followingList;
+  }
+
   public ArrayList<String> getFollowerList() {
     return followerList;
+  }
+
+  public void setFollowerList(ArrayList<String> followerList) {
+    this.followerList = followerList;
   }
 
   public ArrayList<String> getFollowRequests() {
     return followRequests;
   }
 
+  public void setFollowRequests(ArrayList<String> followRequests) {
+    this.followRequests = followRequests;
+  }
+
   public String getAccountType() {
     return accountType;
+  }
+
+  public void setAccountType(String accountType) {
+    this.accountType = accountType;
   }
 }
