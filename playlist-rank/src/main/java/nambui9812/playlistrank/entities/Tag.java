12a72c0c4525c378711955a3c90d33b9a5a7482b
nambui9812 <buihainam9812@gmail.com>
2020-02-20 11:04:04 -0700

    create tag entity

---
+++ playlist-rank/src/main/java/nambui9812/playlistrank/entities/Tag.java
@@ -0,0 +1,54 @@
+package nambui9812.playlistrank.entities;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Tag {
+  private @Id String id;
+  private String name;
+  private ArrayList<String> likes;
+  private ArrayList<String> dislikes;
+
+  // Default constructor
+  public Tag() {}
+
+  // Custom constructor
+  public Tag(
+    String name,
+    ArrayList<String> likes,
+    ArrayList<String> dislikes
+  ) {
+    this.name = name;
+    this.likes = likes;
+    this.dislikes = dislikes;
+  }
+
+  // All get methods
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ArrayList<String> getLikes() {
+    return likes;
+  }
+
+  public void setLikes(ArrayList<String> likes) {
+    this.likes = likes;
+  }
+
+  public ArrayList<String> getDislikes() {
+    return dislikes;
+  }
+
+  public void setDislikes(ArrayList<String> dislikes) {
+    this.dislikes = dislikes;
+  }
+}
