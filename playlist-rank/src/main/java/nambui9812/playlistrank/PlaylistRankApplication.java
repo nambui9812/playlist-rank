5d58aa639220b0f0923a6a8dac989333a78f18d1
nambui9812 <buihainam9812@gmail.com>
2020-02-27 13:51:26 -0700

    add context mongodb validation

--- playlist-rank/src/main/java/nambui9812/playlistrank/PlaylistRankApplication.java
+++ playlist-rank/src/main/java/nambui9812/playlistrank/PlaylistRankApplication.java
@@ -2,12 +2,25 @@ package nambui9812.playlistrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class PlaylistRankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaylistRankApplication.class, args);
+	}
+
+	@Bean
+	public ValidatingMongoEventListener validatingMongoEventListener() {
+		return new ValidatingMongoEventListener(validator());
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		return new LocalValidatorFactoryBean();
 	}
 
 }
