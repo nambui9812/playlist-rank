34001d7a8ea4fbdde6e994e7db2b82f1a110ba31
nambui9812 <buihainam9812@gmail.com>
2020-03-07 15:17:09 -0700

    fixing

--- playlist-rank/src/main/java/nambui9812/playlistrank/PlaylistRankApplication.java
+++ playlist-rank/src/main/java/nambui9812/playlistrank/PlaylistRankApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
 public class PlaylistRankApplication {
 
 	public static void main(String[] args) {
